4dc12944f496b570d65e6ab7ae68da50a6bebadb
Alain CAO <alain.cao@sigmaconso.com>
2018-01-19 20:46:34 +0100

    Cookies & session ID

--- Startup.cs
+++ Startup.cs
@@ -11,10 +11,16 @@ namespace TestWebSiteCore
 {
 	public class Startup
 	{
+		public const string		SessionKey	= "SomeSessionKey";
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddSession( options=>
+				{
+					options.IdleTimeout = TimeSpan.FromMinutes( 30 );
+				} );
 			services.AddMvc();
 		}
 
@@ -25,6 +31,26 @@ namespace TestWebSiteCore
 			{
 				app.UseDeveloperExceptionPage();
 			}
+
+			app.UseSession();
+			app.Use( async (context,next)=>
+				{
+					var sessionID = context.Session.GetString( SessionKey );
+					if( string.IsNullOrWhiteSpace(sessionID) )
+					{
+						// Generate a securely pseudo-random sessionID (cf. ASP's SessionID)
+						// The aim is to have a string that uniquely identifies the user across it's different page requests
+						// => The resulting cookie must NOT be cryptographically guessable or another user could spoof it's identity
+						// E.g. example below in case the cookies are not encrypted
+						var tokenData = new byte[32];
+						var rnd = new System.Security.Cryptography.RNGCryptoServiceProvider();
+						rnd.GetBytes( tokenData );
+						sessionID = Convert.ToBase64String( tokenData );
+
+						context.Session.SetString( SessionKey, sessionID );
+					}
+					await next();
+				} );
 
 			app.UseStaticFiles();
 			app.UseMvc();
